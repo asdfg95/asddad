5f50375c0a0704460efb9fd34eb4045252e6cbbc
Tanya1795 <dimapetrushin1405@rambler.ru>
2016-03-04 00:23:40 +0300

    task1209

---
+++ qw1/qw1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qw1
+{
+
+    class main
+    {
+        public static void Main()
+        {
+            int n, i, c, j, l; string rez = ""; string t = "";
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                c = Convert.ToInt32(Console.ReadLine());
+                t = "1";
+                l = t.Length;
+                for (j = 1; l <= c; j++)
+                {
+                    t = t + Convert.ToString(Math.Pow(10, j));
+                    l = t.Length;
+                }
+                rez = rez + t[c - 1] + " ";
+            }
+            Console.WriteLine(rez);
+            Console.ReadLine();
+        }
+
+    }
+}
