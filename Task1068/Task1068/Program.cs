5b3e1e3cde037fc168a0a323a3c055794e609d64
Tanya1795 <dimapetrushin1405@rambler.ru>
2016-03-08 01:19:39 +0300

    Task1068

---
+++ Task1068/Task1068/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, p = 0;
+            x = int.Parse(Console.ReadLine());
+
+            if (x < 0 && x >= -10000)
+            {
+                for (int i = x; i <= 1; i++)
+                    p += i;
+            }
+            else if (x > 0 && x <= 10000)
+            {
+                for (int i = 1; i <= x; i++)
+                    p += i;
+            }
+            else
+                p = 1;
+
+            Console.WriteLine(p);
+
+            Console.ReadKey();
+        }
+    }
+}
