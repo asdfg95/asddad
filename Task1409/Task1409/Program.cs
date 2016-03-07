86c78d31fb808cb801b696c9f3ed7898a0028633
Tanya1795 <dimapetrushin1405@rambler.ru>
2016-03-08 01:10:01 +0300

    Task1409

---
+++ Task1409/Task1409/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Two_Gangsters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] x = s.Split(' ');
+            int H = Convert.ToInt32(x[0]);
+            int L = Convert.ToInt32(x[1]);
+
+            Console.WriteLine((L - 1) + " " + (H - 1));
+
+            Console.ReadLine();
+        }
+    }
+}
